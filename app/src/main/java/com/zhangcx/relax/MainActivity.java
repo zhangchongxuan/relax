9c1eb3e237b1f112e77ceef63dfa1e4ab4640c3f
zhangchongxuan <zhangcx770>
2018-01-17 23:45:23 +0800

    //add android studio3.0  productFlavors

--- app/src/main/java/com/zhangcx/relax/MainActivity.java
+++ app/src/main/java/com/zhangcx/relax/MainActivity.java
@@ -10,8 +10,11 @@ public class MainActivity extends AppCompatActivity {
     private static final boolean IS_DEBUG = BuildConfig.IS_DEBUG;
     private static final String SERVER_URL0 = BuildConfig.SERVER_URL0;
     private static final String SERVER_URL1 = BuildConfig.SERVER_URL1;
+    private static final String which = BuildConfig.which;
+
     private TextView tv_url;
     private TextView tv_isDebug;
+    private TextView tv_isFree;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +26,14 @@ public class MainActivity extends AppCompatActivity {
     private void initViews() {
         tv_url=(TextView)   findViewById(R.id.tv_url);
         tv_isDebug=(TextView)  findViewById(R.id.tv_isDebug);
+        tv_isFree=(TextView)  findViewById(R.id.tv_isFree);
 
         tv_url.setText(SERVER_URL0+SERVER_URL1);
 
         tv_isDebug.setText(""+IS_DEBUG);
+
+
+        tv_isFree.setText(which);
 
 
         Logger.e("tkl");
