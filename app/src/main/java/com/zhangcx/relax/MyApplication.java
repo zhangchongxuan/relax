473b9840f82ad4c16924b9937207d3751bd4a32c
zhangchongxuan <zhangcx770>
2018-04-16 09:00:10 +0800

    //add android studio3.0  productFlavors

--- app/src/main/java/com/zhangcx/relax/MyApplication.java
+++ app/src/main/java/com/zhangcx/relax/MyApplication.java
@@ -1,11 +1,21 @@
 package com.zhangcx.relax;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+import android.text.TextUtils;
 
 import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.FormatStrategy;
 import com.orhanobut.logger.Logger;
 import com.orhanobut.logger.PrettyFormatStrategy;
+import com.tencent.bugly.Bugly;
+import com.tencent.bugly.beta.Beta;
+import com.tencent.bugly.crashreport.CrashReport;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  * Created by zhangchongxuan on 18/1/10.
@@ -15,6 +25,8 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+
         String  packName=getPackageName();
 
         FormatStrategy formatStrategy = PrettyFormatStrategy.newBuilder()
@@ -32,5 +44,64 @@ public class MyApplication extends Application {
                 return BuildConfig.IS_DEBUG;
             }
         });
+
+//        Bugly.init(this, "953d125a82", true);
+
+
+
+
+        Context context = getApplicationContext();
+// 获取当前包名
+        String packageName = context.getPackageName();
+// 获取当前进程名
+        String processName = getProcessName(android.os.Process.myPid());
+// 设置是否为上报进程
+        CrashReport.UserStrategy strategy = new CrashReport.UserStrategy(context);
+        strategy.setUploadProcess(processName == null || processName.equals(packageName));
+// 初始化Bugly
+
+//        CrashReport.initCrashReport(getApplicationContext(), "953d125a82", true,strategy);//初始化,测试为true  显示日志
+
+        Bugly.init(this, "953d125a82", true,strategy);
+
+    }
+
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+
+        // 安装tinker
+        Beta.installTinker();
+
+    }
+
+
+    /**
+     * 获取进程号对应的进程名
+     *
+     * @param pid 进程号
+     * @return 进程名
+     */
+    private static String getProcessName(int pid) {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("/proc/" + pid + "/cmdline"));
+            String processName = reader.readLine();
+            if (!TextUtils.isEmpty(processName)) {
+                processName = processName.trim();
+            }
+            return processName;
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
+        return null;
     }
 }
--- mylibrary/proguard-rules.pro
+++ mylibrary/proguard-rules.pro
@@ -19,8 +19,3 @@
 # If you keep the line number information, uncomment this to
 # hide the original source file name.
 #-renamesourcefileattribute SourceFile
--dontwarn com.tencent.bugly.**
--keep public class com.tencent.bugly.**{*;}
-
-# 避免影响升级功能，需要keep住support包的类
--keep class android.support.**{*;}
